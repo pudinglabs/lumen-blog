34006bbcf089f4b4c6242767342887389a29e2e9
pudinglabs <>
2021-04-07 02:49:03 +0700

    categories model

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Category extends Eloquent
+{
+    protected $table = 'categories';
+    
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+}
