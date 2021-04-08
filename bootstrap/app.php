c6c863c49648fc6f3431f592391999b90f3155cd
pudinglabs <>
2021-04-08 11:25:25 +0700

    Sentry Integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -98,6 +98,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Sentry\Laravel\ServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
